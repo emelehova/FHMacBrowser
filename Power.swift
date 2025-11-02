9e39e1349fa87c5073b4ab5307c29bd5acd714e2
emelehova <103140191+emelehova@users.noreply.github.com>
2025-11-03 01:48:58 +0300

    Create Power.swift

---
+++ Power.swift
@@ -0,0 +1,20 @@
+import Foundation
+import IOKit.pwr_mgt
+
+enum Power {
+    private static var assertionID: IOPMAssertionID = 0
+
+    static func keepAwakeOn() {
+        IOPMAssertionCreateWithName(kIOPMAssertionTypeNoDisplaySleep as CFString,
+                                    IOPMAssertionLevel(kIOPMAssertionLevelOn),
+                                    "FHMacBrowser Active" as CFString,
+                                    &assertionID)
+    }
+
+    static func keepAwakeOff() {
+        if assertionID != 0 {
+            IOPMAssertionRelease(assertionID)
+            assertionID = 0
+        }
+    }
+}
